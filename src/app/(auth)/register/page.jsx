71b2cd7b442a6e337c548c0f5cf57047cbd2e9cf
Rakibul Islam <mdraislam56@gmail.com>
2026-08-26 17:36:55 +0600

    frist Commit

---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RegisterPage = () => {
+    return (
+        <div className='text-red-550'>
+            this register page
+        </div>
+    );
+};
+
+export default RegisterPage;
